5926f368de1aa72278bf0c4939a135bf450949e1
7hundredtech <ojosamuel_700@yahoo.com>
2023-10-14 22:59:09 +0100

    update sex

--- mine.py
+++ mine.py
@@ -1,7 +1,8 @@
 # Define variables
 name = "Gbenga Ojo-samuel"
 email = 'ojosamuel700@gmail.com'
-
+sex = "male"
 # Print the name and email address
 print("Name:", name)
 print("Email:", email)
+print ("sex :", sex)
